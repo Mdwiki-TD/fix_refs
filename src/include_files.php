4535cf0c219984eb7e61ddc853aa1a483c7dd0f5
ibrahem <ibrahem.al-radaei@outlook.com>
2025-03-01 01:26:47 +0300

    Update include_files.php

--- src/include_files.php
+++ src/include_files.php
@@ -4,12 +4,15 @@ include_once __DIR__ . '/WikiParse/include_it.php';
 
 include_once __DIR__ . '/mv_dots.php';
 
-include_once __DIR__ . '/bots/remove_duplicate_refs.php';
-include_once __DIR__ . '/bots/expend_refs.php';
-include_once __DIR__ . '/bots/es_months.php';
-include_once __DIR__ . '/bots/es_refs.php';
-include_once __DIR__ . '/bots/fix_pt_months.php';
-include_once __DIR__ . '/bots/txtlib2.php';
+foreach (glob(__DIR__ . "/bots/*.php") as $filename) {
+    include_once $filename;
+}
+// include_once __DIR__ . '/bots/remove_duplicate_refs.php';
+// include_once __DIR__ . '/bots/expend_refs.php';
+// include_once __DIR__ . '/bots/es_months.php';
+// include_once __DIR__ . '/bots/es_refs.php';
+// include_once __DIR__ . '/bots/fix_pt_months.php';
+// include_once __DIR__ . '/bots/txtlib2.php';
 
 include_once __DIR__ . '/sw.php';
 include_once __DIR__ . '/md_cat.php';
