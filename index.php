16c4587dd0ba605c8780747ccc5f71632a62f15e
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-06-01 02:51:02 +0300

    Add new text processing functionality and improve index.php layout; implement wikitext fetching and enhance user input handling

--- index.php
+++ index.php
@@ -1,14 +1,112 @@
+<!DOCTYPE html>
+<HTML lang=en dir=ltr data-bs-theme="light" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="robots" content="noindex">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Tools</title>
+    <link href='https://tools-static.wmflabs.org/cdnjs/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <script src='https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.7.0/jquery.min.js'></script>
+    <script src='https://tools-static.wmflabs.org/cdnjs/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js'></script>
+</head>
+
 <?php
 /*
 
-use function WpRefs\FixPage\fix_page_here;
 use function WpRefs\FixPage\DoChangesToText1;
 
 */
 
+$lang         = isset($_GET['lang']) ? trim($_GET['lang']) : '';
+$mdwiki_revid        = isset($_GET['revid']) ? trim($_GET['revid']) : '';
+$sourcetitle  = isset($_GET['sourcetitle']) ? trim($_GET['sourcetitle']) : '';
+$title  = isset($_GET['title']) ? trim($_GET['title']) : '';
+
 include_once __DIR__ . '/work.php';
+include_once __DIR__ . '/wikibots/wikitext.php';
 
-use function WpRefs\FixPage\fix_page_here;
 use function WpRefs\FixPage\DoChangesToText1;
+use function WpRefs\WikiText\get_wikipedia_text;
 
-// $text = DoChangesToText1($sourcetitle, $text, $lang, $mdwiki_revid);
+echo "
+    <body>
+        <div id='maindiv' class='container mt-3'>
+            <div class='card'>
+                <div class='card-header aligncenter' style='font-weight:bold;'>
+                    <!-- input infos -->
+                </div>
+                <div class='card-body'>
+";
+
+
+if (empty($lang) || empty($mdwiki_revid) || empty($sourcetitle)) {
+    // عرض نموذج لإرسال البيانات إلى text_changes.php
+    echo <<<HTML
+        <form action='index.php' method='GET'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col-md-3'>
+                        <div class='input-group mb-3'>
+                            <div class='input-group-prepend'>
+                                <span class='input-group-text'>Langcode</span>
+                            </div>
+                            <input class='form-control' type='text' name='lang' id='lang' value='ja' required />
+                        </div>
+                    </div>
+                    <div class='col-md-3'>
+                        <div class='input-group mb-3'>
+                            <div class='input-group-prepend'>
+                                <span class='input-group-text'>title</span>
+                            </div>
+                            <input class='form-control' type='text' id='title' name='title' value='利用者:Doc James/Rh血液型不適合' />
+                        </div>
+                    </div>
+                    <div class='col-md-3'>
+                        <div class='input-group mb-3'>
+                            <div class='input-group-prepend'>
+                                <span class='input-group-text'>sourcetitle</span>
+                            </div>
+                            <input class='form-control' type='text' id='sourcetitle' name='sourcetitle' value='Rhesus disease' required />
+                        </div>
+                    </div>
+                    <div class='col-md-3'>
+                        <div class='input-group mb-3'>
+                            <div class='input-group-prepend'>
+                                <span class='input-group-text'>revid</span>
+                            </div>
+                            <input class='form-control' type='text' id='revid' name='revid' value='1457313' required />
+                        </div>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class='col-md-3'>
+                        <h4 class='aligncenter'>
+                            <input class='btn btn-outline-primary' type='submit' value='start'>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </form>
+    HTML;
+} else {
+    $text = get_wikipedia_text($title, $lang);
+    $new_text = DoChangesToText1($sourcetitle, $title, $text, $lang, $mdwiki_revid);
+    $new_text_sanitized = htmlspecialchars($new_text, ENT_QUOTES, 'UTF-8');
+    $no_changes = (trim($new_text) === trim($text)) ? "true" : "false";
+    echo <<<HTML
+    <h2>New Text: (no_changes: $no_changes)</h2>
+        <textarea name="new_text" rows="15" cols="140">$new_text_sanitized</textarea>
+    HTML;
+}
+
+echo <<<HTML
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+HTML;
+
+?>
---
+++ text_post.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once __DIR__ . '/work.php';
+
+use function WpRefs\FixPage\DoChangesToText1;
+
+$lang         = isset($_POST['lang']) ? trim($_POST['lang']) : '';
+$text         = isset($_POST['text']) ? trim($_POST['text']) : '';
+$mdwiki_revid = isset($_POST['revid']) ? trim($_POST['revid']) : '';
+$sourcetitle  = isset($_POST['sourcetitle']) ? trim($_POST['sourcetitle']) : '';
+$title        = isset($_POST['title']) ? trim($_POST['title']) : '';
+
+if (!empty($lang) && !empty($text) && !empty($title)) {
+    // استدعاء الدالة التي تجري التعديلات على النص
+    $new_text = DoChangesToText1($sourcetitle, $title, $text, $lang, $mdwiki_revid);
+    if ($new_text === $text) {
+        echo 'no changes';
+        return;
+    }
+    echo $new_text;
+} else {
+    echo 'no text';
+}
