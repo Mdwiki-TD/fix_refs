d4f29797bf2ee847cff33ab4096222668803c655
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-06-01 03:02:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
-<!DOCTYPE html>
-<HTML lang=en dir=ltr data-bs-theme="light" xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="robots" content="noindex">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Tools</title>
-    <link href='https://tools-static.wmflabs.org/cdnjs/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
-    <script src='https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.7.0/jquery.min.js'></script>
-    <script src='https://tools-static.wmflabs.org/cdnjs/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js'></script>
-</head>
-
 <?php
 /*
 
 use function WpRefs\FixPage\DoChangesToText1;
 
 */
+$header_path = __DIR__ . '/header.php';
+if (!file_exists($header_path)) {
+    // "I:\mdwiki\mdwiki\public_html\header.php"
+    $header_path = __DIR__ . '/../mdwiki/public_html/header.php';
+}
+
+include_once $header_path;
 
 $lang         = isset($_GET['lang']) ? trim($_GET['lang']) : '';
 $mdwiki_revid        = isset($_GET['revid']) ? trim($_GET['revid']) : '';
@@ -31,17 +24,15 @@ use function WpRefs\FixPage\DoChangesToText1;
 use function WpRefs\WikiText\get_wikipedia_text;
 
 echo "
-    <body>
-        <div id='maindiv' class='container mt-3'>
-            <div class='card'>
-                <div class='card-header aligncenter' style='font-weight:bold;'>
-                    <!-- input infos -->
-                </div>
-                <div class='card-body'>
+    <div class='card'>
+        <div class='card-header aligncenter' style='font-weight:bold;'>
+            <h3>Fix references in Wikipedia: <a href='https://hashtags.wmcloud.org/?query=mdwiki' target='_blank'>#mdwiki</a></h3>
+        </div>
+        <div class='card-body'>
 ";
 
 
-if (empty($lang) || empty($mdwiki_revid) || empty($sourcetitle)) {
+if (empty($lang) || empty($title)) {
     // عرض نموذج لإرسال البيانات إلى text_changes.php
     echo <<<HTML
         <form action='index.php' method='GET'>
@@ -66,9 +57,9 @@ if (empty($lang) || empty($mdwiki_revid) || empty($sourcetitle)) {
                     <div class='col-md-3'>
                         <div class='input-group mb-3'>
                             <div class='input-group-prepend'>
-                                <span class='input-group-text'>sourcetitle</span>
+                                <span class='input-group-text'>mdwiki title</span>
                             </div>
-                            <input class='form-control' type='text' id='sourcetitle' name='sourcetitle' value='Rhesus disease' required />
+                            <input class='form-control' type='text' id='sourcetitle' name='sourcetitle' value='' />
                         </div>
                     </div>
                     <div class='col-md-3'>
@@ -76,7 +67,7 @@ if (empty($lang) || empty($mdwiki_revid) || empty($sourcetitle)) {
                             <div class='input-group-prepend'>
                                 <span class='input-group-text'>revid</span>
                             </div>
-                            <input class='form-control' type='text' id='revid' name='revid' value='1457313' required />
+                            <input class='form-control' type='text' id='revid' name='revid' value='' />
                         </div>
                     </div>
                 </div>
@@ -97,7 +88,7 @@ if (empty($lang) || empty($mdwiki_revid) || empty($sourcetitle)) {
     $no_changes = (trim($new_text) === trim($text)) ? "true" : "false";
     echo <<<HTML
     <h2>New Text: (no_changes: $no_changes)</h2>
-        <textarea name="new_text" rows="15" cols="140">$new_text_sanitized</textarea>
+        <textarea name="new_text" rows="15" cols="100">$new_text_sanitized</textarea>
     HTML;
 }
 
@@ -108,5 +99,3 @@ echo <<<HTML
     </body>
 </html>
 HTML;
-
-?>
