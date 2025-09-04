5199778d432aaf55686429686d80c004ea8f9e28
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-05 01:21:01 +0300

    ئ

--- src/WikiParse/src/ParserTags.php
+++ src/WikiParse/src/ParserTags.php
@@ -60,7 +60,7 @@ class ParserTags
         );
 
         foreach ($standardMatches as $match) {
-            echo "\n$match[0]\n";
+            // echo "\n$match[0]\n";
             $matches[] = [
                 'original'    => $match[0],
                 'name'        => $match['tag'],
