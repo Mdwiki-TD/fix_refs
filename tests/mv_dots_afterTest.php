966c5e30576d1427dca6c726867e45cd827bf8a9
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-14 02:14:48 +0300

    ئ

--- tests/mv_dots_afterTest.php
+++ tests/mv_dots_afterTest.php
@@ -4,6 +4,7 @@ include_once __DIR__ . '/../../src/include_files.php';
 
 use PHPUnit\Framework\TestCase;
 use function WpRefs\MovesDots\move_dots_after_refs;
+use function WpRefs\MovesDots\remove_spaces_between_last_word_and_beginning_of_ref;
 
 class mv_dots_afterTest extends TestCase
 {
@@ -239,5 +240,17 @@ class mv_dots_afterTest extends TestCase
         $input = 'Հետծննդյան հոգեբանական խանգարումը հանդիպում է 1000 ծննդաբերությունից 1-2-ի մոտ։ <ref name="Os2018" /><ref name="Li2018" /> Տարբեր [[Մշակույթ|մշակույթներում]] և [[Դասակարգային կառուցվածք|սոցիալական դասերում]] գները նման են թվում։ <ref name="Luc2021" /> Ավելի հաճախ այն հանդիպում է հայտնի կամ նոր սկսվող երկբևեռ խանգարման համատեքստում, որը հայտնի է որպես հետծննդյան երկբևեռ խանգարում : <ref name="Luc2021" /> Այս վիճակը նկարագրվել է դեռևս մ.թ.ա. 400 թվականից [[Հիպոկրատ|Հիպոկրատի]] կողմից ։ <ref name="Os2018" />\n\n== test ==';
         $expected = 'Հետծննդյան հոգեբանական խանգարումը հանդիպում է 1000 ծննդաբերությունից 1-2-ի մոտ<ref name="Os2018" /><ref name="Li2018" />։ Տարբեր [[Մշակույթ|մշակույթներում]] և [[Դասակարգային կառուցվածք|սոցիալական դասերում]] գները նման են թվում<ref name="Luc2021" />։ Ավելի հաճախ այն հանդիպում է հայտնի կամ նոր սկսվող երկբևեռ խանգարման համատեքստում, որը հայտնի է որպես հետծննդյան երկբևեռ խանգարում : <ref name="Luc2021" /> Այս վիճակը նկարագրվել է դեռևս մ.թ.ա. 400 թվականից [[Հիպոկրատ|Հիպոկրատի]] կողմից<ref name="Os2018" />։\n\n== test ==';
         $this->assertEqualCompare($expected, $input, move_dots_after_refs($input, 'hy'));
+    }
+    public function testRemoveSpaceEnd()
+    {
+        $input = <<<'TXT'
+            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։
+            == test ==
+            TXT;
+        $expected = <<<'TXT'
+            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից<ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։
+            == test ==
+            TXT;
+        $this->assertEqualCompare($expected, $input, remove_spaces_between_last_word_and_beginning_of_ref($input, 'hy'));
     }
 }
