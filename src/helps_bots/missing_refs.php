2e7112c3acc5799366e4edb9c7e9b43050f68612
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-14 08:23:48 +0300

    Add missing_refs functionality and corresponding tests
    - Implemented fix_missing_refs function to handle empty references.
    - Added missing_refs.php to define the new functionality.
    - Updated index.php to utilize fix_missing_refs.
    - Created missing_refsTest.php to test the new functionality.
    - Enhanced run.bat to filter PHPUnit tests for missing_refsTest.

---
+++ src/helps_bots/missing_refs.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace WpRefs\MissingRefs;
+
+/*
+usage:
+
+use function WpRefs\MissingRefs\fix_missing_refs;
+
+*/
+
+define("DEBUG", TRUE);
+
+use function WpRefs\TestBot\echo_debug;
+use function WpRefs\Parse\Reg_Citations\getShortCitations;
+use function WpRefs\Parse\Reg_Citations\get_full_refs;
+use function WpRefs\Parse\Citations\getCitationsOld;
+
+function refs_expend($short_refs, $text, $alltext)
+{
+    $refs = get_full_refs($alltext);
+
+    foreach ($short_refs as $cite) {
+        $name = $cite["name"];
+        $refe = $cite["tag"];
+        // ---
+        $rr = $refs[$name] ?? false;
+        if ($rr) {
+            echo_debug("refs_expend: $name");
+            // ---
+            $text = str_replace($refe, $rr, $text);
+        }
+    }
+    return $text;
+}
+
+function get_full_text($mdwiki_revid)
+{
+    // ---
+    $path = "/data/project/medwiki";
+    // ---
+    if (substr(__DIR__, 0, 2) == 'I:') {
+        $path = "I:/medwiki/new/medwiki.toolforge.org_repo";
+    };
+    // ---
+    $file = "$path/public_html/revisions_new/$mdwiki_revid/wikitext.txt";
+    // ---
+    if (!file_exists($file)) {
+        echo_debug("file not found: $file");
+        return "";
+    };
+    // ---
+    return file_get_contents($file);
+}
+
+function find_empty_short($text)
+{
+    $shorts = getShortCitations($text);
+    $fulls = get_full_refs($text);
+    $empty_refs = [];
+    foreach ($shorts as $cite) {
+        $name = $cite["name"];
+        // ---
+        $rr = $fulls[$name] ?? false;
+        if (!$rr) {
+            $empty_refs[$name] = $cite;
+        }
+    }
+    // ---
+    return $empty_refs;
+}
+
+function fix_missing_refs($text, $sourcetitle, $mdwiki_revid)
+{
+    $empty_short = find_empty_short($text);
+    // ---
+    echo_debug("empty refs: " . count($empty_short));
+    // ---
+    if (empty($empty_short)) return $text;
+    // ---
+    $full_text = get_full_text($mdwiki_revid);
+    // ---
+    if (empty($full_text)) return $text;
+    // ---
+    $text = refs_expend($empty_short, $text, $full_text);
+    // ---
+    return $text;
+}
