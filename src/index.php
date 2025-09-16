fd72e860f3846435614d109b2dbc1963f015ed94
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-17 00:10:51 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -23,6 +23,7 @@ use function WpRefs\MdCat\add_Translated_from_MDWiki;
 use function WpRefs\Bots\Mini\mini_fixes;
 use function WpRefs\Bots\Mini\mini_fixes_after_fixing;
 use function WpRefs\RemoveSpace\remove_spaces_between_last_word_and_beginning_of_ref;
+use function WpRefs\RemoveSpace\remove_spaces_between_ref_and_punctuation;
 use function WpRefs\MissingRefs\fix_missing_refs;
 
 
@@ -73,6 +74,7 @@ function fix_page($text, $title, $move_dots, $infobox, $add_en_lang, $lang, $sou
     // ---
     if ($lang === "hy") {
         $text = remove_spaces_between_last_word_and_beginning_of_ref($text, "hy");
+        $text = remove_spaces_between_ref_and_punctuation($text);
     }
     // ---
     $text = add_Translated_from_MDWiki($text, $lang);
