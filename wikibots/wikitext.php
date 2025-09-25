5c1ab712166a03462a80a20d749d924f81426fb7
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-25 05:30:26 +0300

    Update wikitext.php

--- wikibots/wikitext.php
+++ wikibots/wikitext.php
@@ -26,12 +26,12 @@ function from_api($title, $lang)
         'titles' => $title,
     ];
 
-    // echo $url . '?' . http_build_query($data) . "<br>";
+    // echo $url . '?' . http_build_query($data, '', '&', PHP_QUERY_RFC3986) . "<br>";
 
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&', PHP_QUERY_RFC3986));
     $response = curl_exec($ch);
     curl_close($ch);
 
