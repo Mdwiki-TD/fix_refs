9cd9399daadf13fc2ef846ddfdb4d2a4e0942182
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-05 02:17:05 +0300

    Update section.php

--- src/es_bots/section.php
+++ src/es_bots/section.php
@@ -11,17 +11,19 @@ use function WpRefs\EsBots\Section\es_section;
 
 function es_section($sourcetitle, $text, $mdwiki_revid)
 {
-    // ---
+    // Replace old template with new one
     // replace ({{Traducido ref|mdwiki|) with ({{Traducido ref MDWiki|en|)
-    $text = preg_replace('/\{\{\s*Traducido\s*ref\s*\|\s*mdwiki\s*\|/iu', "{{Traducido ref MDWiki|en|", $text);
-    // ---
-    // if text has /\{\{\s*Traducido ref\s*\|/ then return text
-    preg_match('/\{\{\s*Traducido\s*ref\s*MDWiki\s*\|/iu', $text, $ma);
-    if (!empty($ma)) {
-        // pub_test_print("return text;");
+    $text = preg_replace(
+        '/\{\{\s*Traducido\s*ref\s*\|\s*mdwiki\s*\|/iu',
+        "{{Traducido ref MDWiki|en|",
+        $text
+    );
+
+    // If template already exists (any variant), return as-is
+    if (preg_match('/\{\{\s*Traducido\s*ref(?:\s*MDWiki)?\s*\|/iu', $text)) {
         return $text;
     }
-    // ---
+
     $date = "{{subst:CURRENTDAY}} de {{subst:CURRENTMONTHNAME}} de {{subst:CURRENTYEAR}}";
     // ---
     // $temp = "{{Traducido ref|mdwiki|$sourcetitle|oldid=$mdwiki_revid|trad=|fecha=$date}}";
