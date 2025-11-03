24b698102f4c4471cfb1e1d9e6892bca08b46e70
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-04 00:36:00 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 
+vendor/bin/phpunit tests --testdox --colors=always -c phpunit.xml
 vendor/bin/phpunit tests --testdox --colors=always -c phpunit.xml --filter missing_refsTest
 vendor/bin/phpstan analyse
