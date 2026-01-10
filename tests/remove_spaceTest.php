c12baed38339ca0abc001aedaa773b5edcc36ae6
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-10 03:31:46 +0300

    Refactor tests to use inline input and expected values
    Replaced file-based input and expected values with inline heredoc strings in testRemoveSpaceEnd3rdFile, testRemoveSpaceEnd4thFile, and testRemoveSpaceEnd5thFile. Commented out file I/O operations to simplify and make tests more self-contained.

--- tests/remove_spaceTest.php
+++ tests/remove_spaceTest.php
@@ -34,48 +34,64 @@ class remove_spaceTest extends MyFunctionTest
     public function testRemoveSpaceEnd3rdFile()
     {
         $input   = <<<WIKI
-Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
-|<!!>
-}}</ref>։ test1 <ref name="Os2018" /><ref>{{ref
-|<!!>
-}}</ref>։
-WIKI;
+            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
+            |<!!>
+            }}</ref>։ test1 <ref name="Os2018" /><ref>{{ref
+            |<!!>
+            }}</ref>։
+        WIKI;
         $expected   = <<<WIKI
-Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
-|<!!>
-}}</ref>։ test1<ref name="Os2018" /><ref>{{ref
-|<!!>
-}}</ref>։
-WIKI;
+            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
+            |<!!>
+            }}</ref>։ test1<ref name="Os2018" /><ref>{{ref
+            |<!!>
+            }}</ref>։
+        WIKI;
         // --
         $result = remove_spaces_between_last_word_and_beginning_of_ref($input, 'hy');
-        // --
-        $output_file   = __DIR__ . "/texts/remove_space_texts/3/output.txt";
-        file_put_contents($output_file, $result);
         // --
         $this->assertEqualCompare($expected, $input, $result);
     }
     public function testRemoveSpaceEnd4thFile()
     {
-        $input   = file_get_contents(__DIR__ . "/texts/remove_space_texts/4/input.txt");
-        $expected   = file_get_contents(__DIR__ . "/texts/remove_space_texts/4/expected.txt");
+        $input   = <<<WIKI
+            Հետծննդյան հոգեբանական խանգարումը հանդիպում է 1000 ծննդաբերությունից 1-2-ի մոտ։ <ref name="Os2018" /><ref name="Li2018" /> Տարբեր [[Մշակույթ|մշակույթներում]] և [[Դասակարգային կառուցվածք|սոցիալական դասերում]] գները նման են թվում։ <ref name="Luc2021" /> Ավելի հաճախ այն հանդիպում է հայտնի կամ նոր սկսվող երկբևեռ խանգարման համատեքստում, որը հայտնի է որպես հետծննդյան երկբևեռ խանգարում : <ref name="Luc2021" /> Այս վիճակը նկարագրվել է դեռևս մ.թ.ա. 400 թվականից [[Հիպոկրատ|Հիպոկրատի]] կողմից ։ <ref name="Os2018" />
+        WIKI;
+        $expected   = <<<WIKI
+            Հետծննդյան հոգեբանական խանգարումը հանդիպում է 1000 ծննդաբերությունից 1-2-ի մոտ։ <ref name="Os2018" /><ref name="Li2018" /> Տարբեր [[Մշակույթ|մշակույթներում]] և [[Դասակարգային կառուցվածք|սոցիալական դասերում]] գները նման են թվում։ <ref name="Luc2021" /> Ավելի հաճախ այն հանդիպում է հայտնի կամ նոր սկսվող երկբևեռ խանգարման համատեքստում, որը հայտնի է որպես հետծննդյան երկբևեռ խանգարում : <ref name="Luc2021" /> Այս վիճակը նկարագրվել է դեռևս մ.թ.ա. 400 թվականից [[Հիպոկրատ|Հիպոկրատի]] կողմից ։ <ref name="Os2018" />
+        WIKI;
         // --
         $result = remove_spaces_between_last_word_and_beginning_of_ref($input, 'hy');
-        // --
-        $output_file   = __DIR__ . "/texts/remove_space_texts/4/output.txt";
-        file_put_contents($output_file, $result);
         // --
         $this->assertEqualCompare($expected, $input, $result);
     }
     public function testRemoveSpaceEnd5thFile()
     {
-        $input   = file_get_contents(__DIR__ . "/texts/remove_space_texts/5/input.txt");
-        $expected   = file_get_contents(__DIR__ . "/texts/remove_space_texts/5/expected.txt");
+        // $input   = file_get_contents(__DIR__ . "/texts/remove_space_texts/5/input.txt");
+        // $expected   = file_get_contents(__DIR__ . "/texts/remove_space_texts/5/expected.txt");
         // --
+        $input   = <<<WIKI
+            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref
+            |<!!>
+            }}</ref>։ կողմից <ref name="Os2018" /><ref>{{ref
+            |zz
+            }}</ref>։
+
+            == test ==
+        WIKI;
+        $expected   = <<<WIKI
+            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref
+            |<!!>
+            }}</ref>։ կողմից<ref name="Os2018" /><ref>{{ref
+            |zz
+            }}</ref>։
+
+            == test ==
+        WIKI;
         $result = remove_spaces_between_last_word_and_beginning_of_ref($input, 'hy');
         // --
-        $output_file   = __DIR__ . "/texts/remove_space_texts/5/output.txt";
-        file_put_contents($output_file, $result);
+        // $output_file   = __DIR__ . "/texts/remove_space_texts/5/output.txt";
+        // file_put_contents($output_file, $result);
         // --
         $this->assertEqualCompare($expected, $input, $result);
     }
