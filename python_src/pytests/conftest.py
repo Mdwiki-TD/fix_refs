e8596a6e20864a51569e37b1978af022d98bfcff
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-21 05:04:50 +0300

    Update conftest.py

--- python_src/pytests/conftest.py
+++ python_src/pytests/conftest.py
@@ -6,5 +6,5 @@ from pathlib import Path
 project_root = Path(__file__).parent.parent
 
 # Add python_src to sys.path so we can import from 'src' as a package
-python_src_path = project_root / "python_src"
+python_src_path = project_root# / "python_src"
 sys.path.insert(0, str(python_src_path))
