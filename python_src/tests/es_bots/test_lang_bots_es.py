ba0f1598a590cd24bc12d3b9a8dcc96cbacb2681
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-24 01:20:22 +0300

    Move Spanish month tests to test_lang_bots_es.py
    The Spanish month translation tests from test_es_months.py were moved into test_lang_bots_es.py for consolidation. The original test_es_months.py file was deleted. Also, the expected output in one test was updated to match the input formatting.

--- python_src/tests/es_bots/test_lang_bots_es.py
+++ python_src/tests/es_bots/test_lang_bots_es.py
@@ -14,6 +14,31 @@ from src.lang_bots.es_helpers import (
 from src.bots.months import make_date_new_val_es
 
 
+class TestEsMonths:
+    """Test cases for Spanish month translation"""
+
+    def test_make_date_new_val_es_with_full_date(self):
+        """Test converting full date with month to Spanish"""
+        assert make_date_new_val_es("July 25, 1975") == "25 de julio de 1975"
+
+    def test_temp_in_wikitexts(self):
+        """Test fixing months in references within wikitext"""
+        input_text = ('test: <ref name="AHFS2016">{{Citar web|titulo=Charcoal, Activated|url=https://www.drugs.com/monograph/charcoal-activated.html|publicado=The American Society of Health-System Pharmacists|acessodata=8 December 2016|urlmorta=live|arquivourl=https://web.archive.org/web/20161221011707/https://www.drugs.com/monograph/charcoal-activated.html|arquivodata=21 December 2016}}</ref>\n'
+                      '<ref name="AHFS2016">{{Citar web|acessodata=8 December 2016|urlmorta=live|arquivourl=https://web.archive.org/web/20161221011707/https://www.drugs.com/monograph/charcoal-activated.html|arquivodata=21 December 2016}} xxxxxxxxxxxxxxxx {{Webarchive|url=https://web.archive.org/web/20161221011707/https://www.drugs.com/monograph/charcoal-activated.html|date=21 December 2016}}</ref>')
+
+        expected = ('test: <ref name="AHFS2016">{{Citar web|titulo=Charcoal, Activated|url=https://www.drugs.com/monograph/charcoal-activated.html|publicado=The American Society of Health-System Pharmacists|acessodata=8 de diciembre de 2016|urlmorta=live|arquivourl=https://web.archive.org/web/20161221011707/https://www.drugs.com/monograph/charcoal-activated.html|arquivodata=21 de diciembre de 2016}}</ref>\n'
+                    '<ref name="AHFS2016">{{Citar web|acessodata=8 de diciembre de 2016|urlmorta=live|arquivourl=https://web.archive.org/web/20161221011707/https://www.drugs.com/monograph/charcoal-activated.html|arquivodata=21 de diciembre de 2016}} xxxxxxxxxxxxxxxx {{Webarchive|url=https://web.archive.org/web/20161221011707/https://www.drugs.com/monograph/charcoal-activated.html|date=21 de diciembre de 2016}}</ref>')
+
+        result = fix_es_months_in_refs(input_text)
+        assert result == expected
+
+    def test_temp_in_ref(self):
+        """Test fixing months in single reference"""
+        input_text = '<ref name="test" group="notes">{{cite web|date=25  December 2016 |}}</ref>'
+        expected = '<ref name="test" group="notes">{{cite web|date=25 de diciembre de 2016|}}</ref>'
+        result = fix_es_months_in_refs(input_text)
+        assert result == expected
+
 class TestSpanishHelpers:
     """Test cases for Spanish language helpers"""
 
@@ -43,7 +68,7 @@ class TestSpanishHelpers:
     def test_temp_in_templates_more(self):
         """Test fixing months in multiple templates"""
         input_text = '{{cite web|date=10 January, 2023|}} {{cite book|time = test|date = 10 de enero de 2023 }}'
-        expected = '{{cite web|date=10 de enero de 2023|}} {{cite book|time=test|date=10 de enero de 2023}}'
+        expected = '{{cite web|date=10 de enero de 2023|}} {{cite book|time = test|date = 10 de enero de 2023 }}'
         result = fix_es_months_in_texts(input_text)
         assert result == expected
 
